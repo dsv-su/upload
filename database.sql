b6203290aa8d08246c887b3ca14c62391200ce02
Erik Thuning <boooink@gmail.com>
2022-04-22 17:13:40 +0200

    New format database dump

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,6 +21,20 @@ CREATE TABLE `items` (
   `upload_time`    bigint(20)  DEFAULT NULL,
   `end_time`       bigint(20)  NOT NULL,
   PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `sharing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sharing` (
+  `item`           varchar(64) NOT NULL,
+  `user`           varchar(64) NOT NULL,
+  PRIMARY KEY (`item`,`user`),
+  CONSTRAINT `sharing_item_uuid`
+    FOREIGN KEY (`item`)
+    REFERENCES `items` (`uuid`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
